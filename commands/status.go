4b821bed367e5d505e5cd22337624cb4795d420f
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-03-30 15:12:16 -0400

    chore: set status message
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- commands/status.go
+++ commands/status.go
@@ -16,8 +16,13 @@ const (
 	StatusSetCommand = "status-set"
 )
 
-func StatusSet(runner CommandRunner, status Status) error {
-	_, err := runner.Run(StatusSetCommand, string(status))
+func StatusSet(runner CommandRunner, status Status, message string) error {
+	var args []string
+	args = append(args, string(status))
+	if message != "" {
+		args = append(args, message)
+	}
+	_, err := runner.Run(StatusSetCommand, args...)
 	if err != nil {
 		return fmt.Errorf("failed to set status: %w", err)
 	}
