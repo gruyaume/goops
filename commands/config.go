4b0dba0360c4a44b54518d5418ce2c6db88f5f20
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-03-30 15:41:24 -0400

    feat: support any type of config option
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- commands/config.go
+++ commands/config.go
@@ -9,13 +9,13 @@ const (
 	ConfigGetCommand = "config-get"
 )
 
-func ConfigGet(runner CommandRunner, key string) (string, error) {
+func ConfigGet(runner CommandRunner, key string) (any, error) {
 	args := []string{key, "--format=json"}
 	output, err := runner.Run(ConfigGetCommand, args...)
 	if err != nil {
 		return "", fmt.Errorf("failed to get config: %w", err)
 	}
-	var configValue string
+	var configValue any
 	err = json.Unmarshal(output, &configValue)
 	if err != nil {
 		return "", fmt.Errorf("failed to parse config value: %w", err)
--- commands/config_test.go
+++ commands/config_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gruyaume/goops/commands"
 )
 
-func TestConfigGet_Success(t *testing.T) {
+func TestConfigGetString_Success(t *testing.T) {
 	fakeRunner := &FakeRunner{
 		Output: []byte(`"banana"`),
 		Err:    nil,
@@ -20,6 +20,32 @@ func TestConfigGet_Success(t *testing.T) {
 		t.Fatalf("Expected %q, got %q", "banana", result)
 	}
 
+	if fakeRunner.Command != commands.ConfigGetCommand {
+		t.Errorf("Expected command %q, got %q", commands.ConfigGetCommand, fakeRunner.Command)
+	}
+	if len(fakeRunner.Args) != 2 {
+		t.Fatalf("Expected 2 arguments, got %d", len(fakeRunner.Args))
+	}
+	if fakeRunner.Args[0] != "fruit" {
+		t.Errorf("Expected argument %q, got %q", "fruit", fakeRunner.Args[0])
+	}
+	if fakeRunner.Args[1] != "--format=json" {
+		t.Errorf("Expected argument %q, got %q", "--format=json", fakeRunner.Args[1])
+	}
+}
+
+func TestConfigGetFloat64_Success(t *testing.T) {
+	fakeRunner := &FakeRunner{
+		Output: []byte(`1234`),
+		Err:    nil,
+	}
+	result, err := commands.ConfigGet(fakeRunner, "fruit")
+	if err != nil {
+		t.Fatalf("ConfigGet returned an error: %v", err)
+	}
+	if result != float64(1234) {
+		t.Fatalf("Expected %f, got %f", 1234.0, result)
+	}
 	if fakeRunner.Command != commands.ConfigGetCommand {
 		t.Errorf("Expected command %q, got %q", commands.ConfigGetCommand, fakeRunner.Command)
 	}
