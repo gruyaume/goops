9b0aaa55682997202c83a7523b54e6fc12854e68
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-05-15 16:24:21 +0200

    feat: simplify retrieving charm metadata (#32)
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- context.go
+++ context.go
@@ -3,16 +3,24 @@ package goops
 import (
 	"github.com/gruyaume/goops/commands"
 	"github.com/gruyaume/goops/environment"
+	"github.com/gruyaume/goops/metadata"
 )
 
 type HookContext struct {
 	Commands    *commands.Command
 	Environment *environment.Environment
+	Metadata    *metadata.Metadata
 }
 
 func NewHookContext() *HookContext {
+	env := environment.NewEnvironment()
+
+	charmDir := env.JujuCharmDir()
+	metadataPath := charmDir + "/metadata.yaml"
+
 	return &HookContext{
 		Commands:    commands.NewCommand(),
-		Environment: environment.NewEnvironment(),
+		Environment: env,
+		Metadata:    metadata.GetCharmMetadata(metadataPath),
 	}
 }
