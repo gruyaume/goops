a34cb58849fedac5ba8ef18a15123316fdfdfd6c
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-06-16 10:30:41 -0400

    feat: GetConfig accepts config struct
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- config.go
+++ config.go
@@ -12,76 +12,24 @@ const (
 
 var ErrConfigNotSet = errors.New("config option not set")
 
-func GetConfig(key string) (any, error) {
+func GetConfig(config any) error {
 	commandRunner := GetRunner()
 
-	args := []string{key, "--format=json"}
+	args := []string{"--format=json"}
 
 	output, err := commandRunner.Run(configGetCommand, args...)
 	if err != nil {
-		return "", fmt.Errorf("failed to get config: %w", err)
+		return fmt.Errorf("failed to get config: %w", err)
 	}
 
-	var configValue any
+	if len(output) == 0 {
+		return ErrConfigNotSet
+	}
 
-	err = json.Unmarshal(output, &configValue)
+	err = json.Unmarshal(output, config)
 	if err != nil {
-		return "", fmt.Errorf("failed to parse config value: %w", err)
+		return fmt.Errorf("failed to parse config: %w", err)
 	}
 
-	return configValue, nil
-}
-
-func GetConfigString(key string) (string, error) {
-	value, err := GetConfig(key)
-	if err != nil {
-		return "", err
-	}
-
-	if value == nil {
-		return "", ErrConfigNotSet
-	}
-
-	strValue, ok := value.(string)
-	if !ok {
-		return "", fmt.Errorf("config value is not a string: %v", value)
-	}
-
-	return strValue, nil
-}
-
-func GetConfigInt(key string) (int, error) {
-	value, err := GetConfig(key)
-	if err != nil {
-		return 0, err
-	}
-
-	if value == nil {
-		return 0, ErrConfigNotSet
-	}
-
-	floatValue, ok := value.(float64)
-	if !ok {
-		return 0, fmt.Errorf("config value is not a number: %v", value)
-	}
-
-	return int(floatValue), nil
-}
-
-func GetConfigBool(key string) (bool, error) {
-	value, err := GetConfig(key)
-	if err != nil {
-		return false, err
-	}
-
-	if value == nil {
-		return false, ErrConfigNotSet
-	}
-
-	boolValue, ok := value.(bool)
-	if !ok {
-		return false, fmt.Errorf("config value is not a bool: %v", value)
-	}
-
-	return boolValue, nil
+	return nil
 }
--- config_test.go
+++ config_test.go
@@ -6,148 +6,20 @@ import (
 	"github.com/gruyaume/goops"
 )
 
-func TestConfigGet_Success(t *testing.T) {
-	fakeRunner := &FakeRunner{
-		Output: []byte(`"banana"`),
-		Err:    nil,
-	}
-
-	goops.SetRunner(fakeRunner)
-
-	result, err := goops.GetConfig("fruit")
-	if err != nil {
-		t.Fatalf("ConfigGet returned an error: %v", err)
-	}
-
-	if _, ok := result.(string); !ok {
-		t.Fatalf("Expected result to be a string, got %T", result)
-	}
-
-	if result != "banana" {
-		t.Fatalf("Expected %q, got %q", "banana", result)
-	}
-
-	if fakeRunner.Command != "config-get" {
-		t.Errorf("Expected command %q, got %q", "config-get", fakeRunner.Command)
-	}
-
-	if len(fakeRunner.Args) != 2 {
-		t.Fatalf("Expected 2 arguments, got %d", len(fakeRunner.Args))
-	}
-
-	if fakeRunner.Args[0] != "fruit" {
-		t.Errorf("Expected argument %q, got %q", "fruit", fakeRunner.Args[0])
-	}
-
-	if fakeRunner.Args[1] != "--format=json" {
-		t.Errorf("Expected argument %q, got %q", "--format=json", fakeRunner.Args[1])
-	}
+type MyConfig struct {
+	Color    string `json:"color"`
+	Quantity int    `json:"quantity"`
+	ForSale  bool   `json:"for_sale"`
 }
 
-func TestConfigGetString_Success(t *testing.T) {
-	fakeRunner := &FakeRunner{
-		Output: []byte(`"banana"`),
-		Err:    nil,
+func TestBla(t *testing.T) {
+	var dat MyConfig
+
+	if err := goops.GetConfig(&dat); err != nil {
+		panic(err)
 	}
 
-	goops.SetRunner(fakeRunner)
-
-	result, err := goops.GetConfigString("fruit")
-	if err != nil {
-		t.Fatalf("ConfigGetString returned an error: %v", err)
-	}
-
-	if result != "banana" {
-		t.Fatalf("Expected %q, got %q", "banana", result)
-	}
-}
-
-func TestConfigGetString_BadType(t *testing.T) {
-	fakeRunner := &FakeRunner{
-		Output: []byte(`123`),
-		Err:    nil,
-	}
-
-	goops.SetRunner(fakeRunner)
-
-	_, err := goops.GetConfigString("fruit")
-	if err == nil {
-		t.Fatalf("Expected error, got nil")
-	}
-
-	if err.Error() != "config value is not a string: 123" {
-		t.Fatalf("Expected error %q, got %q", "config value is not a string: 123", err.Error())
-	}
-}
-
-func TestConfigGetInt_Success(t *testing.T) {
-	fakeRunner := &FakeRunner{
-		Output: []byte(`123`),
-		Err:    nil,
-	}
-
-	goops.SetRunner(fakeRunner)
-
-	result, err := goops.GetConfigInt("fruit")
-	if err != nil {
-		t.Fatalf("ConfigGetInt returned an error: %v", err)
-	}
-
-	if result != 123 {
-		t.Fatalf("Expected %d, got %d", 123, result)
-	}
-}
-
-func TestConfigGetInt_BadType(t *testing.T) {
-	fakeRunner := &FakeRunner{
-		Output: []byte(`"banana"`),
-		Err:    nil,
-	}
-
-	goops.SetRunner(fakeRunner)
-
-	_, err := goops.GetConfigInt("fruit")
-	if err == nil {
-		t.Fatalf("Expected error, got nil")
-	}
-
-	if err.Error() != "config value is not a number: banana" {
-		t.Fatalf("Expected error %q, got %q", "config value is not a number: banana", err.Error())
-	}
-}
-
-func TestConfigGetBool_Success(t *testing.T) {
-	fakeRunner := &FakeRunner{
-		Output: []byte(`true`),
-		Err:    nil,
-	}
-
-	goops.SetRunner(fakeRunner)
-
-	result, err := goops.GetConfigBool("fruit")
-	if err != nil {
-		t.Fatalf("ConfigGetBool returned an error: %v", err)
-	}
-
-	if result != true {
-		t.Fatalf("Expected %t, got %t", true, result)
-	}
-}
-
-func TestConfigGetBool_BadType(t *testing.T) {
-	fakeRunner := &FakeRunner{
-		Output: []byte(`123`),
-		Err:    nil,
-	}
-
-	goops.SetRunner(fakeRunner)
-
-	_, err := goops.GetConfigBool("fruit")
-	if err == nil {
-		t.Fatalf("Expected error, got nil")
-	}
-
-	if err.Error() != "config value is not a bool: 123" {
-		t.Fatalf("Expected error %q, got %q", "config value is not a bool: 123", err.Error())
+	if dat.Color != "red" {
+		t.Fatalf("Expected color 'red', got '%s'", dat.Color)
 	}
 }
