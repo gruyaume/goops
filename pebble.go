55af8ca0c3fa873b445bec1db6313a42f525b035
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-06-16 17:04:14 -0400

    feat: add pebble support
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

---
+++ pebble.go
@@ -0,0 +1,50 @@
+package goops
+
+import (
+	"fmt"
+
+	"github.com/canonical/pebble/client"
+)
+
+var defaultPebbleGetter PebbleGetter = realPebbleGetter{}
+
+type PebbleGetter interface {
+	Pebble(container string) PebbleClient
+}
+
+func Pebble(container string) PebbleClient {
+	return defaultPebbleGetter.Pebble(container)
+}
+
+type PebbleClient interface {
+	Stop(opts *client.ServiceOptions) (changeID string, err error)
+	WaitChange(changeID string, options *client.WaitChangeOptions) (*client.Change, error)
+	Exec(opts *client.ExecOptions) (PebbleExecProcess, error)
+	// ... and all the other pebble.Client methods
+}
+
+type PebbleExecProcess interface {
+	Wait() error
+	SendResize(width int, height int) error
+	SendSignal(signal string) error
+}
+
+type realPebbleGetter struct{}
+
+func (g realPebbleGetter) Pebble(container string) PebbleClient {
+	pebble, err := client.New(&client.Config{
+		Socket: fmt.Sprintf("/charm/containers/%s/pebble.socket", container),
+	})
+	if err != nil {
+		panic(err) // shouldn't happen
+	}
+	return &realPebbleStub{pebble}
+}
+
+type realPebbleStub struct {
+	*client.Client
+}
+
+func (p *realPebbleStub) Exec(opts *client.ExecOptions) (PebbleExecProcess, error) {
+	return p.Client.Exec(opts)
+}
